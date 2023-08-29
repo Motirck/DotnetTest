feaa577f5e77e1a9f38bf388a26f13a4538746f8
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2023-08-29 19:10:56 -0300

    feat: Fixing third issue

--- Coding_challenge/ClimateMonitor/Services/AlertService.cs
+++ Coding_challenge/ClimateMonitor/Services/AlertService.cs
@@ -7,14 +7,14 @@ public class AlertService
     private static readonly HashSet<Func<DeviceReadingRequest, Alert?>> SensorValidators = new()
     {
         deviceReading =>
-            deviceReading.Humidity is < 0 or > 100 
-            ? new Alert(AlertType.HumiditySensorOutOfRange, "Humidity sensor is out of range.")
-            : default,
+            deviceReading.Humidity is < 0 or > 100
+                ? new Alert(AlertType.HumiditySensorOutOfRange, "Humidity sensor is out of range.")
+                : default,
 
-        deviceReading => 
-            deviceReading.Humidity is < -10 or > 50 
-            ? new Alert(AlertType.TemperatureSensorOutOfRange, "Temperature sensor is out of range.")
-            : default,
+        deviceReading =>
+            deviceReading.Temperature is < -10 or > 50
+                ? new Alert(AlertType.TemperatureSensorOutOfRange, "Temperature sensor is out of range.")
+                : default,
     };
 
     public IEnumerable<Alert> GetAlerts(DeviceReadingRequest deviceReadingRequest)
